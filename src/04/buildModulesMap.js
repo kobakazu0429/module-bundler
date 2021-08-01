15306f385bf4ee8dabca55b1688960c90cf6ed86
kobakazu0429 <kobakazu10@gmail.com>
2021-08-01 21:03:21 +0900

    split index.js to some files

---
+++ src/04/buildModulesMap.js
@@ -0,0 +1,67 @@
+"use strict";
+const { readFileSync } = require("fs");
+const { default: traverse } = require("@babel/traverse");
+const { parse } = require("../03/utils/parser");
+const { getScriptFilePath } = require("../01/pathUtils");
+const { resolveModulePath } = require("./utils/module");
+
+async function buildModulesMap(entryDir, entryFilename) {
+  const modulesMap = new Set();
+  const entryPath = getScriptFilePath(entryDir, entryFilename);
+  const visitedFiles = [];
+
+  // start from the entry-point to check all deps
+  walkDeps(entryDir, entryPath, entryFilename);
+
+  function walkDeps(_currentDir, beforePath, src) {
+    const { nextRoot, filePath } = resolveModulePath(src, entryDir, beforePath);
+
+    if (visitedFiles.includes(filePath)) {
+      return;
+    }
+
+    visitedFiles.push(filePath);
+
+    try {
+      const ast = parse(readFileSync(filePath, "utf-8"));
+      let type = "esm";
+
+      traverse(ast, {
+        // import
+        ImportDeclaration({ node: { type, source } }) {
+          type = "esm";
+          walkDeps(nextRoot, filePath, source.value);
+        },
+
+        // require
+        CallExpression({ node: { callee, arguments: args } }) {
+          if (callee.type === "Identifier" && callee.name === "require") {
+            type = "cjs";
+            walkDeps(nextRoot, filePath, args[0].value);
+          }
+        },
+
+        // check ESM or CJS
+        // exports or module.exports or ESM export
+        ExpressionStatement({ node: { expression } }) {
+          if (expression.operator === "=") {
+            type = "cjs";
+          }
+        },
+      });
+
+      modulesMap.add({
+        id: modulesMap.size,
+        ast,
+        path: filePath,
+        type, // in fact, CJS as default is better
+      });
+    } catch (e) {
+      console.warn("could not find the module:", filePath);
+    }
+  }
+
+  return modulesMap;
+}
+
+module.exports = { buildModulesMap };
--- src/04/index.js
+++ src/04/index.js
@@ -1,102 +1,11 @@
 "use strict";
 
-const { promises, readFileSync, existsSync } = require("fs");
+const { promises, existsSync } = require("fs");
 const { dirname, basename, join } = require("path");
-const { default: traverse } = require("@babel/traverse");
-const { default: generate } = require("@babel/generator");
 const { transformAst } = require("./transformAst");
-const { mainTemplate, moduleTemplate } = require("./template");
-const { parse } = require("../03/utils/parser");
-const { getScriptFilePath } = require("../01/pathUtils");
-const { getModule, resolveModulePath } = require("./utils/module");
-
-async function buildModulesMap(entryDir, entryFilename) {
-  const modulesMap = new Set();
-  const entryPath = getScriptFilePath(entryDir, entryFilename);
-  const visitedFiles = [];
-
-  // start from the entry-point to check all deps
-  walkDeps(entryDir, entryPath, entryFilename);
-
-  function walkDeps(_currentDir, beforePath, src) {
-    const { nextRoot, filePath } = resolveModulePath(src, entryDir, beforePath);
-
-    if (visitedFiles.includes(filePath)) {
-      return;
-    }
-
-    visitedFiles.push(filePath);
-
-    try {
-      const ast = parse(readFileSync(filePath, "utf-8"));
-      let type = "esm";
-
-      traverse(ast, {
-        // import
-        ImportDeclaration({ node: { type, source } }) {
-          type = "esm";
-          walkDeps(nextRoot, filePath, source.value);
-        },
-
-        // require
-        CallExpression({ node: { callee, arguments: args } }) {
-          if (callee.type === "Identifier" && callee.name === "require") {
-            type = "cjs";
-            walkDeps(nextRoot, filePath, args[0].value);
-          }
-        },
-
-        // check ESM or CJS
-        // exports or module.exports or ESM export
-        ExpressionStatement({ node: { expression } }) {
-          if (expression.operator === "=") {
-            type = "cjs";
-          }
-        },
-      });
-
-      modulesMap.add({
-        id: modulesMap.size, // the entry point's id is 0
-        ast,
-        path: filePath, // an absolute path
-        type, // in fact, CJS as default is better
-      });
-    } catch (e) {
-      console.warn("could not find the module:", filePath);
-    }
-  }
-
-  return modulesMap;
-}
-
-// replace a module path with a moduleId
-function convertToModuleId(basePath, modulesMap) {
-  const modules = new Map();
-
-  for (const { id, ast, path } of modulesMap.values()) {
-    traverse(ast, {
-      CallExpression({ node: { callee, arguments: args } }) {
-        if (callee.type === "Identifier" && callee.name === "require") {
-          const { id: moduleId } = getModule(
-            modulesMap,
-            path,
-            args[0].value,
-            basePath
-          );
-
-          args[0].value = moduleId;
-        }
-      },
-    });
-
-    modules.set(id, {
-      path,
-      code: moduleTemplate(generate(ast).code),
-    });
-  }
-
-  return modules;
-}
+const { mainTemplate } = require("./template");
+const { buildModulesMap } = require("./buildModulesMap");
+const { convertToModuleId } = require("./convertToModuleId");
 
 async function bundler({ entry, outputPath }) {
   const entryFilename = basename(entry);
